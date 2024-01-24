30a7765dd339b182398a3a3a371f090cb2cc744a
“mahmoudjiha” <107807116+mahmoudjiha@users.noreply.github.com>
2024-01-24 07:40:34 -0800

    1/23/24

---
+++ case_statement.rb
@@ -0,0 +1,14 @@
+# case_statement.rb
+
+a = 5
+
+output_string = case a
+    when 5
+        "a is 5"
+    when 6
+        "a is 6"
+    else
+        "a is neither 5, nor 6"
+    end
+
+puts output_string
